82d44f1050ee9649d0dd200521b5a5afc30f4bb8
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2022-07-10 23:45:40 +0800

    Fix the display problem so that the first digit zero will be replaced if another number is entered

--- script.js
+++ script.js
@@ -27,26 +27,60 @@ function operate(operator, x, y) {
     }
 }
 
-let displayValue = '';
-let hasDefaultZero = true;
+const OPERATORS = ['+', '-', '×', '÷'];
+let displayValue = '0';
+let firstDigitIsZero = true;
+let wasNumber = true;
 const display = document.getElementById('display');
 
+
+
 /**
- * Used for displaying numbers.
+ * Displays numbers and operators and save them for later evaluation.
+ *
  */
-function displayNumber() {
-    if (hasDefaultZero && this.textContent !== '0') {
-        hasDefaultZero = false;
-    }
-    if (!hasDefaultZero) {
-        displayValue += this.textContent;
+function displayToken() {
+    let token = this.textContent;
+
+    if (Number.isInteger(Number(token))) {
+        if (firstDigitIsZero) {  // For convenience, give the calculator a default value of
+            if (token !== '0') {    // 0. If the user inputs another number, replace the zero
+                firstDigitIsZero = false;  // by it.
+            }
+            displayValue = displayValue.substring(0, displayValue.length - 1);
+        }
+        if (!wasNumber && token === '0') {  // record if user inputs a zero after an operator
+            firstDigitIsZero = true;
+        }
+
+        displayValue += token;
         display.textContent = `${displayValue}`;
+        wasNumber = true;
+    } else if (!OPERATORS.includes(displayValue.charAt(displayValue.length - 1)) && wasNumber) {
+        if (isSecondOp()) {
+
+        }
+        displayValue += token;
+        display.textContent = `${displayValue}`;
+        wasNumber = false;
     }
 }
 
+
+
+function isSecondOp() {
+    const expression = displayValue.split('');
+    expression.forEach(token => {
+        if (OPERATORS.includes(token)) {
+            return true;
+        }
+    });
+    return false;
+}
 function clearDisplay() {
-    display.textContent = '';
-    displayValue = '';
+    display.textContent = '0';
+    displayValue = '0';
+
 }
 
 const precedence = {
@@ -56,53 +90,19 @@ const precedence = {
     '÷': 1,
 }
 
-/**
- * Evaluates infix expression and returns the result.
- */
-function evaluate() {
-    let token = '';
-    let valueStack = [];
-    let opStack = [];
-    let wasNumber = false;
-    for (let i = 0; i < displayValue.length; i++) {
-        token = displayValue.charAt(i);
-        if (wasNumber && Number.isInteger(Number(token))) {
-            valueStack[valueStack.length - 1] += token;
-        } else if (Number.isInteger(Number(token))) {
-            wasNumber = true;
-            valueStack.push(token);
-        } else {
-            wasNumber = false;
-            while (opStack.length > 0 && precedence[opStack[opStack.length - 1]]
-                >= precedence[`${token}`]) {
-                let secondVal = valueStack.pop();
-                valueStack.push(operate(opStack.pop(), valueStack.pop(), secondVal));
-            }
-            opStack.push(token);
-        }
-    }
-    while (opStack.length > 0) {
-        let secondVal = valueStack.pop();
-        valueStack.push(operate(opStack.pop(), valueStack.pop(), secondVal));
-    }
-    return valueStack[0];
-}
 
 /**
  * For simplicity, use this function to Hook up buttons to event listeners.
  */
 function setUp() {
     const numbers = Array.from(document.querySelectorAll('.number'));
-    numbers.forEach((number) => number.addEventListener('click', displayNumber));
+    numbers.forEach((number) => number.addEventListener('click', displayToken));
 
     const operators = Array.from(document.querySelectorAll('.operator'));
-    operators.forEach((operator) => operator.addEventListener('click', displayNumber));
+    operators.forEach((operator) => operator.addEventListener('click', displayToken));
 
     const equals = document.getElementById('equals-button');
-    equals.addEventListener('click', () => {
-        displayValue = evaluate();
-        display.textContent = `${displayValue}`;
-    });
+
 
     const clear = document.getElementById('clear-button');
     clear.addEventListener('click', clearDisplay);
